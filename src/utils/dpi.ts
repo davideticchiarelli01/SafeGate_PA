eb63efbc985305704baf5803afce1800b41f64c0
davideticchiarelli01 <davideticchiarelli01@gmail.com>
2025-06-02 16:21:10 +0200

    dpi list

---
+++ src/utils/dpi.ts
@@ -0,0 +1,11 @@
+export enum DPI {
+    Helmet = 'helmet',
+    SafetyGlasses = 'safety_glasses',
+    Gloves = 'gloves',
+    SafetyShoes = 'safety_shoes',
+    EarProtection = 'ear_protection',
+    ReflectiveVest = 'reflective_vest',
+    Respirator = 'respirator',
+    Harness = 'harness',
+    ProtectiveClothing = 'protective_clothing',
+}
