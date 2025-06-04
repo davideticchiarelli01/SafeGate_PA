ccdcceb5aace75559c3bb12809c07fba6edeb047
giamp109 <95345801+giamp109@users.noreply.github.com>
2025-06-04 08:41:24 +0200

    update route files

--- src/routes/badgeRoute.ts
+++ src/routes/badgeRoute.ts
@@ -1,9 +1,12 @@
-import {Router} from "express";
-import {controllers} from "../dependencies";
+import { Router } from "express";
+import { controllers } from "../dependencies";
+import { adminMiddleware, authMiddleware } from "../middlewares/authMiddleware";
 
 
 const badgeRouter = Router();
-const {badgeController} = controllers;
+const { badgeController } = controllers;
+
+badgeRouter.use(authMiddleware, adminMiddleware)
 
 badgeRouter.get("/badges", badgeController.getAllBadges);
 badgeRouter.get("/badges/:id", badgeController.getBadge);
--- src/routes/transitRoute.ts
+++ src/routes/transitRoute.ts
@@ -1,14 +1,16 @@
-import {Router} from "express";
-import {authMiddleware, adminMiddleware, gateOrAdminMiddleware} from "../middlewares/authMiddleware";
-import {controllers} from "../dependencies";
+import { Router } from "express";
+import { authMiddleware, adminMiddleware, gateOrAdminMiddleware } from "../middlewares/authMiddleware";
+import { controllers } from "../dependencies";
 
 const transitRouter = Router();
-const {transitController} = controllers;
+const { transitController } = controllers;
+
+transitRouter.use(authMiddleware);
 
 transitRouter.get("/transits", transitController.getAllTransits);
-transitRouter.get("/transits/:id", authMiddleware, transitController.getTransit);
-transitRouter.post("/transits", authMiddleware, gateOrAdminMiddleware, transitController.createTransit);
-transitRouter.put("/transits/:id", authMiddleware, adminMiddleware, transitController.updateTransit);
-transitRouter.delete("/transits/:id", authMiddleware, adminMiddleware, transitController.deleteTransit);
+transitRouter.get("/transits/:id", transitController.getTransit);
+transitRouter.post("/transits", gateOrAdminMiddleware, transitController.createTransit);
+transitRouter.put("/transits/:id", adminMiddleware, transitController.updateTransit);
+transitRouter.delete("/transits/:id", adminMiddleware, transitController.deleteTransit);
 
 export default transitRouter;
