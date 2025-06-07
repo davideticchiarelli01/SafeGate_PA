204f3cc779efec2bc57d68bec9420144337100d7
davideticchiarelli01 <davideticchiarelli01@gmail.com>
2025-06-07 15:36:02 +0200

    Tested badge routes, enhanced input validation and cleaning, added comment to badgeMiddleware.ts

--- src/middlewares/loginMiddleware.ts
+++ src/middlewares/loginMiddleware.ts
@@ -1,7 +1,7 @@
-import { ErrorFactory, HttpError } from "../factories/errorFactory";
-import { ReasonPhrases } from "http-status-codes"
-import { body, validationResult } from 'express-validator';
-import { Request, Response, NextFunction } from 'express';
+import {ErrorFactory, HttpError} from "../factories/errorFactory";
+import {ReasonPhrases} from "http-status-codes"
+import {body, validationResult} from 'express-validator';
+import {Request, Response, NextFunction} from 'express';
 
 const emailRegex = /^[a-zA-Z0-9._!?%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
@@ -9,13 +9,16 @@ const emailValidation = body('email')
     .exists().withMessage('Field "email" is required')
     .bail()
     .isString().withMessage('Email must be a string')
+    .bail()
     .trim()
     .matches(emailRegex).withMessage('Email format is invalid');
 
 const passwordValidation = body('password')
     .exists().withMessage('Field "password" is required')
     .bail()
-    .isString().withMessage('Passowrd must be a string');
+    .isString().withMessage('Password must be a string')
+    .bail()
+    .trim();
 
 const handleValidation = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
