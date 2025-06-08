9bc637214b832e960073e8ebabfd9944d8d29fef
giamp109 <95345801+giamp109@users.noreply.github.com>
2025-06-08 11:31:46 +0200

    Added JSDoc to unitOfWork.ts

--- src/utils/unitOfWork.ts
+++ src/utils/unitOfWork.ts
@@ -1,6 +1,16 @@
-import {Sequelize, Transaction} from 'sequelize';
+import { Sequelize, Transaction } from 'sequelize';
 import DatabaseConnection from '../db/database';
 
+/**
+ * Executes a set of database operations within a single transaction.
+ * Ensures that all operations inside the callback are committed atomically,
+ * or rolled back entirely if an error occurs.
+ *
+ * @template T - The return type of the transactional operation.
+ * @param {(tx: Transaction) => Promise<T>} callback - A function that receives the Sequelize transaction and returns a promise.
+ * @returns {Promise<T>} The result of the operation wrapped in a transaction.
+ *
+ */
 export async function unitOfWork<T>(callback: (tx: Transaction) => Promise<T>): Promise<T> {
 
     const sequelize: Sequelize = DatabaseConnection.getInstance();
